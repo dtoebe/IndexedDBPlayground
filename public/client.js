dc36f57381cc338f7651de63db5c063d01b96e1b
Daniel Toebe <dtoebe@protonmail.com>
2023-11-16 08:52:40 -0700

    feat: init commit

---
+++ public/client.js
@@ -0,0 +1,27 @@
+const seedData = [
+    { name: "Foo", desc: "Bar", count: 1 },
+    { name: "Bar", desc: "Foo", count: 3 },
+    { name: "Boo", desc: "Loo", count: 50 },
+    { name: "Loo", desc: "Boo", count: 200 },
+    { name: "ass", desc: "dd", count: 1 },
+    { name: "dfg", desc: "sdf", count: 13 },
+    { name: "cat", desc: "bar", count: 14 },
+    { name: "hat", desc: "bAr", count: 15 },
+    { name: "fat", desc: "baR", count: 16 },
+    { name: "lat", desc: "bAR", count: 17 },
+    { name: "lnng", desc: "BaR", count: 18 },
+    { name: "poo", desc: "poo", count: 19 },
+    { name: "doo", desc: "foo", count: 10 },
+    { name: "moo", desc: "sdf", count: 11 },
+    { name: "err", desc: "ert", count: 12 },
+    { name: "info", desc: "err", count: 13 },
+    { name: "warn", desc: "wrn", count: 14 },
+    { name: "coo", desc: "inf", count: 15 },
+    { name: "koo", desc: "mi6", count: 16 },
+    { name: "mkk", desc: "mi5", count: 17 },
+    { name: "de", desc: "rty", count: 18 },
+];
+
+window.onload = () => {
+    // let request =
+};
